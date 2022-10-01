0c972e3cd06cc1670d3cd4d7ad0036ff2567101d
SebastianMartinezP <sebix3180@gmail.com>
2022-10-01 16:30:59 -0300

    CRUD usuario

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,9 +1,166 @@
 ﻿using Database.Models;
-
+using UserInterface.Business;
 ModelContext context = new ModelContext();
 
 
-Usuario? u = context.Usuario.FirstOrDefault(u => u.Contrasenahashed.Equals("hashed123"));
-Console.WriteLine(u.Rutprofesional);
+#region Testing CRUD usuario
 
+#region Read
+
+//Database.Models.Usuario? usuarioRead =
+//    UserInterface.Business.Usuario.getUsuario("sebas.martinezp@duocuc.cl", "hashed123");
+
+//if (usuarioRead != null)
+//{
+//    Console.WriteLine("OK");
+//    Console.WriteLine(usuarioRead.Idusuario);
+//    Console.WriteLine(usuarioRead.Idperfil);
+//    Console.WriteLine(usuarioRead.Correo);
+//    Console.WriteLine(usuarioRead.Contrasenahashed);
+//    Console.WriteLine(usuarioRead.Ishabilitado);
+//}
+//else
+//{
+//    Console.WriteLine("ERROR");
+//}
+
+#endregion
+
+#region ReadAll
+
+//List<Database.Models.Usuario> usuariosRead =
+//    UserInterface.Business.Usuario.getAllUsuario();
+
+//if (usuariosRead.Any())
+//{
+//    Console.WriteLine("OK");
+//    foreach (var user in usuariosRead)
+//    {
+//        Console.WriteLine(user.Idusuario);
+//        Console.WriteLine(user.Idperfil);
+//        Console.WriteLine(user.Correo);
+//        Console.WriteLine(user.Contrasenahashed);
+//        Console.WriteLine(user.Ishabilitado);
+//    }
+    
+//}
+//else
+//{
+//    Console.WriteLine("ERROR");
+//}
+
+#endregion
+
+#region Create
+
+//Database.Models.Usuario? usuarioCreate = new Database.Models.Usuario()
+//{
+//    Correo = "prueba1234@correo.cl",
+//    Contrasenahashed = "prueba1234",
+//    Ishabilitado = "2",
+//    Rutprofesional = "20.244.632-9",
+//    Idperfil = 1
+
+//};
+//var result = UserInterface.Business.Usuario.Save(usuarioCreate);
+
+//if (result != null)
+//{
+//    if (result == true)
+//    {
+//        Console.WriteLine("OK");
+//        Console.WriteLine(usuarioCreate.Idusuario);
+//        Console.WriteLine(usuarioCreate.Idperfil);
+//        Console.WriteLine(usuarioCreate.Correo);
+//        Console.WriteLine(usuarioCreate.Contrasenahashed);
+//        Console.WriteLine(usuarioCreate.Ishabilitado);
+//    }
+//    else
+//    {
+//        Console.WriteLine("NO GUARDO");
+//    }
+
+//}
+//else
+//{
+//    Console.WriteLine("ERROR");
+//}
+
+#endregion
+
+#region Update
+
+//Database.Models.Usuario? usuarioUpdate = new Database.Models.Usuario()
+//{
+//    Correo = "prueba1234@correo.cl",
+//    Contrasenahashed = "prueba1234",
+//    Ishabilitado = "1",
+//    Rutprofesional = "20.244.632-9",
+//    Idperfil = 1
+
+//};
+//var resultUpdate = UserInterface.Business.Usuario.Update(usuarioUpdate);
+
+//if (resultUpdate != null)
+//{
+//    if (resultUpdate == true)
+//    {
+//        Console.WriteLine("OK");
+//        Console.WriteLine(usuarioUpdate.Idusuario);
+//        Console.WriteLine(usuarioUpdate.Idperfil);
+//        Console.WriteLine(usuarioUpdate.Correo);
+//        Console.WriteLine(usuarioUpdate.Contrasenahashed);
+//        Console.WriteLine(usuarioUpdate.Ishabilitado);
+//    }
+//    else
+//    {
+//        Console.WriteLine("NO Actualizo");
+//    }
+
+//}
+//else
+//{
+//    Console.WriteLine("ERROR");
+//}
+
+#endregion
+
+#region Disable
+
+//Database.Models.Usuario? usuarioDisable = new Database.Models.Usuario()
+//{
+//    Correo = "prueba1234@correo.cl",
+//    Contrasenahashed = "prueba1234",
+//    Ishabilitado = "1",
+//    Rutprofesional = "20.244.632-9",
+//    Idperfil = 1
+
+//};
+//var resultDisable = UserInterface.Business.Usuario.Disable(usuarioDisable);
+
+//if (resultDisable != null)
+//{
+//    if (resultDisable == true)
+//    {
+//        Console.WriteLine("OK");
+//        Console.WriteLine(usuarioDisable.Idusuario);
+//        Console.WriteLine(usuarioDisable.Idperfil);
+//        Console.WriteLine(usuarioDisable.Correo);
+//        Console.WriteLine(usuarioDisable.Contrasenahashed);
+//        Console.WriteLine(usuarioDisable.Ishabilitado);
+//    }
+//    else
+//    {
+//        Console.WriteLine("NO Actualizo");
+//    }
+
+//}
+//else
+//{
+//    Console.WriteLine("ERROR");
+//}
+
+#endregion
+
+#endregion
 
