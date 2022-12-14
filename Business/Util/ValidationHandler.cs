f0859ece691618e11d654649c92c6158176f53bd
sebastianforever1 <msisebastian7>
2022-12-13 21:59:45 -0300

    fix validacion contrato

--- Business/Util/ValidationHandler.cs
+++ Business/Util/ValidationHandler.cs
@@ -12,16 +12,16 @@
 
         public static bool ValidateRut(string rut)
         {
-            bool correctLength = rut.Length == 12 || rut.Length == 11; //"XX.XXX.XXX-X"
-            bool containsDot = rut.Contains('.');
+            bool correctLength = rut.Length == 9 || rut.Length == 10; //"XX.XXX.XXX-X"
+            //bool containsDot = rut.Contains('.');
             bool containsDash = rut.Contains('-');
-            return correctLength && containsDot && containsDash;
+            return correctLength && containsDash;
         }
 
         public static bool ValidateString(string? value)
         {
             bool notNull = value != null;
-            bool correctLength = value?.Length > 3;
+            bool correctLength = value?.Length != 0;
             return notNull && correctLength;
         }
     }
